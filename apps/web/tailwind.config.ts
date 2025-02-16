dab975186057620df5943c59fdee2189a7ae4b33
jose.sousalves@gmail.com <jose.sousalves@gmail.com>
2025-02-16 16:59:06 +0000

    chore(web/ui): tailwind configuration

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,9 +3,23 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx"],
+const config: Config = {
+  content: ["./app/**/*.tsx", "./src/**/*.tsx"],
   presets: [sharedConfig],
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 12s linear infinite",
+        "pulse-scale": "scale 8s ease-in-out infinite",
+      },
+      keyframes: {
+        scale: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+        },
+      },
+    },
+  },
 };
 
 export default config;
