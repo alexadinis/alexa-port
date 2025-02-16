1ec4000bf1a14c7983cd1450f0523793431942d6
jose.sousalves@gmail.com <jose.sousalves@gmail.com>
2025-02-16 17:01:38 +0000

    chore(web): add Navbar with temporary NAV_LINKS

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,9 +1,34 @@
 import "./globals.css";
 import "@repo/ui/styles.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import Logo from "@/icons/Logo";
+import Navbar from "../src/components/Navbar/Navbar";
+
+const NAV_LINKS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About me",
+    href: "/about",
+  },
+  {
+    label: "Projects",
+    href: "/projects",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Alexa Portfolio",
@@ -17,7 +42,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar navLinks={NAV_LINKS} logo={<Logo />} />
+        {children}
+      </body>
     </html>
   );
 }
