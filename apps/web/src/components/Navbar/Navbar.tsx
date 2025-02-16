0f16b81f92b939e76fbdef1b38c63239f9ade34b
jose.sousalves@gmail.com <jose.sousalves@gmail.com>
2025-02-16 17:04:25 +0000

    chore(web): add Navbar markup and styling

---
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+interface NavLink {
+  label: string;
+  href: string;
+}
+
+interface NavbarProps {
+  navLinks: NavLink[];
+  logo: React.ReactNode;
+}
+
+const Navbar = ({ navLinks, logo }: NavbarProps) => {
+  return (
+    <nav className="flex items-center justify-between p-4">
+      <div>{logo}</div>
+      <div className="flex items-center gap-6">
+        {navLinks.map((link) => (
+          <Link key={link.href} href={link.href}>
+            {link.label.toLowerCase()}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
